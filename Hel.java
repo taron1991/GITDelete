19d814166b53346b2e72bb2cc68f211e33503025
Taron Gukasyan <funkadelica@mail.ru>
2024-11-12 14:56:26 +0300

    added new method

--- Hel.java
+++ Hel.java
@@ -1,6 +1,8 @@
 public class Hel {
     public static void main(String[] args) {
         System.out.println("new class");
+        StringBuilder stringBuilder = new StringBuilder("hello");
+        stringBuilder.append("ss").append("a");
     }
     void helloevr(){
         System.out.println();
